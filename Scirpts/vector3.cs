2bd7fca53ae3aa66b2a76021cb6ea72c13b639a9
BenToronto <83716543+BenToronto@users.noreply.github.com>
2022-02-12 14:46:17 -0500

    code for player die vector

---
+++ Scirpts/vector3.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Vect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	bool playerdievect = allint("isdead", false)
+  if(playerdeadvect = true "vectdie", true)
+  {
+    bool vectdie = intall("isdead", dead("false",= true,"allint", isdead("true",x =(1))))
+     }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
